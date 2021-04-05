996147d1da131456f18fec7f6ce5fe124e8cab18
Pranav Dani <pranavpdani@gmail.com>
2021-04-05 23:11:48 +0530

    Add login and Register template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
-from sqlalchemy import create_engine
+from flask_sqlalchemy import sqlalchemy
+from flask import Flask, jsonify, redirect, render_template, request, session
 
 # from models import db
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from flask import Flask, jsonify
-
 # from flask_cors import CORS
 from dotenv import load_dotenv
+
+import requests
 import psycopg2
 import os
 
@@ -21,32 +21,58 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
 app = Flask(__name__)
 
-# engine = create_engine("postgresql://postgres:160402@localhost:5432/track")
-
-# db = scoped_session(sessionmaker(bind=engine))
 
 try:
     con = psycopg2.connect(database=DATABASE, user=DATABASE_USERNAME, password=DATABASE_PASSWORD)
 
     db = con.cursor()
-
+    print("Database Connection Successful")
     # # GET: Fetch all movies from the database
     # @app.route("/")
     # def fetch_all_movies():
     #     db.execute("SELECT * FROM usercategories")
     #     rows = db.fetchall()
     #     print(rows)
-
     #     return jsonify(rows)
-
 
 except:
     print("Error")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST
+    if request.method == "POST":
+        print("TODO")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print("TODO")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    # session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("layout.html")
 
 
 if __name__ == "__main__":
